7a2ac39ab1611b3271907327e1f886f76bd34a69
dilezhuri <dilezhuri@gmail.com>
2023-05-01 11:33:10 +0200

    Change 01.05.2023

--- Dormitory/Program.cs
+++ Dormitory/Program.cs
@@ -4,6 +4,7 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World from Xhoana!");
+        Console.WriteLine("Hello, World from Dile!");
+
     }
 }
